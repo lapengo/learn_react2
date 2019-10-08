717e8c86f0286896a326bbb9dd232fab21ea18f2
Arif Lapengo <mylapengo@gmail.com>
2019-10-08 16:30:04 +0700

    IDR Corner - #8 Router with Error state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
+import Image from './components/Image';
+import Navbar from './components/Navbar';
+
 
 function App() {
+
+  state = {
+    login: false
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <Navbar />
+
+      <div className="container">
+        <div className="row">
+          <Route exact path='/' component={Home} />
+          <Route path='/About' component={About} />
+          <Route path='/Image' rende={() => (
+            this.state.login ? <Image /> : (<Redirect to='/' />)
+          )} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
